1eb437b4f87e7825fae835d28260e4c140136572
jetroalexisHC <jetroalexis.hernandez@alumnos.ulagos.cl>
2024-05-22 20:51:14 -0400

    1_9999_Nested_IF.py

--- digits_conditions_range.py
+++ digits_conditions_range.py
@@ -1,18 +1,19 @@
-# Identificar la cantidad de digitos de un numero ingresado, en un rango desde el numero 1 hasta el 9999:
+# Ingresar un numero e identificar la cantidad de digitos, desde el 1 hasta el 9999:
 
 num=int(input("ingrese un numero "))
 
-if num >= 0 and num < 10:
-    print("el numero tiene solo 1 digito ")
-elif num >= 10 and num < 100:
-    print("el numero tiene 2 digitos ")
-elif num >= 100 and num < 1000:
-    print("el numero tiene 3 digitos ")
-elif num >= 1000 and num < 10000:
-    print("el numero tiene 4 digitos ")
+if num >= 0 and num < 10000:
+
+    if num < 10:
+        print("el numero tiene 1 digito ")
+    else:
+        if num < 100:
+            print("el numero tiene 2 digitos ")
+        else:
+            if num < 1000:
+                print("el numero tiene 3 digitos ")
+            else:
+                if num < 10000:
+                    print("el numero tiene 4 digitos ")
 else:
-    num > 9999 or num < 0
     print("numero fuera de rango ")
-    
-    
-    
