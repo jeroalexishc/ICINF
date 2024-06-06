ea606edd8cc9408528b05e56332ad2e966c472a2
jetroalexisHC <jetroalexis.hernandez@alumnos.ulagos.cl>
2024-06-06 14:53:33 -0400

    Add files via upload

---
+++ control2_P2.py
@@ -0,0 +1,12 @@
+list=[] 
+
+for x in range(7):
+    add=(input("ingrese nombre: "))
+    list.append(add)
+
+for x in list:
+    for i in x:
+        if [-1]=="a":
+        x.remove(list)
+print(list)
+        
